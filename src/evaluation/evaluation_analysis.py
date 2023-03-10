6d9c1f13fa6b0ce3aefe355863d0bd3b50952171
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-03-10 12:58:02 +0100

    Update evaluation_analysis.py
    - Fix formatting.
    - Add functions to plot ROC curves and precision-recall-F1 curves.

--- src/evaluation/evaluation_analysis.py
+++ src/evaluation/evaluation_analysis.py
@@ -3,10 +3,10 @@ from copy import deepcopy
 from typing import Dict, List, Literal, OrderedDict
 import matplotlib.pyplot as plt
 from sklearn.metrics import (
-    auc, classification_report, ConfusionMatrixDisplay,
-    multilabel_confusion_matrix, precision_recall_curve,
-    average_precision_score, RocCurveDisplay, PrecisionRecallDisplay)
+    classification_report, ConfusionMatrixDisplay, multilabel_confusion_matrix,
+    roc_curve, RocCurveDisplay, auc)
 import numpy as np
+from .evaluation import get_cumulative_precision_recall_and_f1
 
 
 def _get_classification_report_dict(y_true: np.ndarray, y_pred: np.ndarray,
@@ -21,7 +21,7 @@ def _get_classification_report_dict(y_true: np.ndarray, y_pred: np.ndarray,
     y_pred : ndarray
         The predicted labels.
     targets : list of str
-        The target labels.
+        The target label names.
 
     Returns
     -------
@@ -44,14 +44,14 @@ def print_classification_report(y_true: np.ndarray, y_pred: np.ndarray,
     y_pred : ndarray
         The predicted labels.
     targets : list of str
-        The target labels.
+        The target label names.
     dataset_name : str
         The name of the dataset used to compute the classification report.
     """
     print(f'Classification report for the {dataset_name} set:')
     print(classification_report(y_true=y_true, y_pred=y_pred,
                                 target_names=targets, zero_division=0))
-    
+
 def plot_targets_classification_statistics(
     y_true: np.ndarray, y_pred: np.ndarray, targets: List[str],
     dataset_name: str) -> None:
@@ -65,16 +65,19 @@ def plot_targets_classification_statistics(
     y_pred : ndarray
         The predicted labels.
     targets : list of str
-        The target labels.
+        The target label names.
     dataset_name : str
         The name of the dataset used to compute the statistics.
     """
     classification_report_dict = _get_classification_report_dict(y_true, y_pred, targets)
 
-    class_report_sorted = sorted([(classification_report_dict[label]['f1-score'], label) 
-                                  for label in classification_report_dict.keys() if label in targets ])
+    class_report_sorted = sorted(
+        [(classification_report_dict[label]['f1-score'], label) 
+         for label in classification_report_dict.keys() if label in targets ])
 
-    class_report_sorted = OrderedDict({label: classification_report_dict[label] for (_, label) in class_report_sorted})
+    class_report_sorted = OrderedDict(
+        {label: classification_report_dict[label]
+         for (_, label) in class_report_sorted})
 
     plt.figure(figsize=(12,6))
     x_axis = 2*np.arange(len(class_report_sorted))
@@ -111,7 +114,7 @@ def plot_confusion_matrices(
     y_pred : ndarray
         The predicted labels.
     targets : list of str
-        The target labels.
+        The target label names.
     dataset_name : str
         The name of the dataset used to compute the confusion matrices.
     normalize_by : 'recall' | 'precision' | 'accuracy', optional
@@ -119,7 +122,7 @@ def plot_confusion_matrices(
     """
     assert normalize_by in ['recall', 'precision', 'accuracy'], \
         'Select one normalization metric among: recall, precision or accuracy.'
-    
+
     confusion_matrices = multilabel_confusion_matrix(y_true, y_pred)
     confusion_matrices = confusion_matrices.astype(np.float32)
     c_ms = deepcopy(confusion_matrices)
@@ -141,7 +144,7 @@ def plot_confusion_matrices(
                                     display_labels=['Current', 'Others'])
         cf.plot(cmap=plt.cm.Blues, values_format=".2f", ax=ax, colorbar=False)
         ax.set_title(label)
-        
+
     fig.tight_layout()
 
     plt.suptitle(f'Confusion matrices of the {dataset_name} set for each ' +
@@ -149,48 +152,123 @@ def plot_confusion_matrices(
 
     plt.show()
 
-def get_precision_recall_statistics(y_true: np.ndarray, pred_scores: np.ndarray, targets: List[str]):
-    precision, recall, average_precision = dict(), dict(), dict()
+def _plot_roc_subplot(y_true: np.ndarray, y_scores: np.ndarray,
+                      targets: List[str], index: int) -> None:
+    """Plot the ROC curve for selected target classes.
+
+    Parameters
+    ----------
+    y_true : ndarray
+        The true labels.
+    y_pred : ndarray
+        The predicted labels scores.
+    targets : list of str
+        The target label names.
+    index : int
+        The current subplot index.
+    """
+    ax = plt.subplot(2, 2, index)
+
+    for class_id in range(index * 4, index * 4 + 4):
+        RocCurveDisplay.from_predictions(
+            y_true[:, class_id],
+            y_scores[:, class_id],
+            name=f"{targets[class_id]}",
+            ax=ax
+        )
+
+def plot_roc_curves(y_true: np.ndarray, y_scores: np.ndarray,
+                    targets: List[str], dataset_name: str) -> None:
+    """Plot the Receiving Operator Curves for the predictions.
+
+    Parameters
+    ----------
+    y_true : ndarray
+        The true labels.
+    y_pred : ndarray
+        The predicted labels scores.
+    targets : list of str
+        The target label names.
+    dataset_name : str
+        The name of the dataset.
+    """
+    fpr, tpr, roc_auc = dict(), dict(), dict()
     for i in range(len(targets)):
-        precision[i], recall[i], _ = precision_recall_curve(y_true[:, i], pred_scores[:, i])
-        average_precision[i] = average_precision_score(y_true[:, i], pred_scores[:, i])
+        fpr[i], tpr[i], _ = roc_curve(y_true[:,i], y_scores[:,i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
 
-    precision["macro"], recall["macro"], _ = precision_recall_curve(
-        y_true.ravel(), pred_scores.ravel()
-    )
-    average_precision["macro"] = average_precision_score(y_true, pred_scores, average="macro")
-    return precision, recall, average_precision
+    fpr_grid = np.linspace(0.0, 1.0, 1000)
 
-def plot_precision_recall_curves(precision, recall, average_precision, targets: List[str], dataset_name: str) -> None:
+    # Interpolate all ROC curves at these points.
+    mean_tpr = np.zeros_like(fpr_grid)
+
+    for i in range(len(targets)):
+        # Linear interpolation.
+        mean_tpr += np.interp(fpr_grid, fpr[i], tpr[i])
+
+    # Average it and compute AUC.
+    mean_tpr /= len(targets)
+
+    fpr["macro"] = fpr_grid
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    print(f"Macro-averaged One-vs-Rest ROC AUC score:\n{roc_auc['macro']:.2f}")
+
+
     plt.figure(figsize=(15,10))
     plt.subplot(2, 2, 1)
-    plt.suptitle(f'Precision-Recall One-vs-Rest curves on the {dataset_name} set')
+    plt.suptitle('Receiver Operating Characteristic One-vs-Rest curves on the'
+                 f' {dataset_name} set')
 
     for i in range(4):
-        ax = plt.subplot(2, 2, i + 1)
-        for class_id in range(i * 4, i * 4 + 4):
-            display = PrecisionRecallDisplay(
-                recall=recall[class_id],
-                precision=precision[class_id],
-                average_precision=average_precision[class_id]
-            )
-            display.plot(ax=ax, name=f"{targets[class_id]}")
-        display = PrecisionRecallDisplay(
-            recall=recall["macro"],
-            precision=precision["macro"],
-            average_precision=average_precision["macro"]
+        _plot_roc_subplot(y_true, y_scores, targets, i + 1)
+        plt.plot(
+            fpr["macro"],
+            tpr["macro"],
+            label=f"macro-average (AUC = {roc_auc['macro']:.2f})",
+            color="navy",
+            linestyle=":",
+            linewidth=4,
         )
-        display.plot(ax=ax, name="Macro-average precision-recall", color="navy", linestyle=":", linewidth=4)
-        ax.set_xlim([-0.1, 1.1])
-        ax.set_ylim([-0.1, 1.1])
-        #ax.set_xticks(np.arange(0., 1.1, .2))
-        #ax.set_yticks(np.arange(0., 1.1, .2))
-        #display.plot(ax=ax, name=f"Precision-recall for class {i}", color=color)
-        #plt.plot([0, 1], [0, 1], "k--", label="Chance level (AUC = 0.5)")
+        plt.plot([0, 1], [0, 1], "k--", label="Chance level (AUC = 0.5)")
 
-        plt.xlabel("precision")
-        plt.ylabel("recall")
-        plt.legend(loc='upper right')
-        #plt.axis("square")
+        plt.xlabel("False Positive Rate")
+        plt.ylabel("True Positive Rate")
+        plt.legend()
+        plt.axis("square")
+    plt.tight_layout()
+    plt.show()
+
+def plot_precision_recall_f1_macro_curves(
+    y_true: np.ndarray, y_scores: np.ndarray, targets: List[str],
+    dataset_name: str) -> None:
+    """Plot the Receiving Operator Curves for the predictions.
+
+    Parameters
+    ----------
+    y_true : ndarray
+        The true labels.
+    y_pred : ndarray
+        The predicted labels scores.
+    targets : list of str
+        The target label names.
+    dataset_name : str
+        The name of the dataset.
+    """
+    s_preds, c_precision, c_recall, c_f1 = \
+        get_cumulative_precision_recall_and_f1(y_true, y_scores)
+    plt.figure(figsize=(20, 15))
+    plt.suptitle(f'Precision, Recall and F1 macro score on the {dataset_name} '
+                 'set according to a threshold per class\n', size=14)
+    for i, l in enumerate(targets):
+        ax = plt.subplot(4, 5, i + 1)
+        ax.set_title(l)
+        ax.plot(s_preds[:,i], c_precision[:,i], label='precision')
+        ax.plot(s_preds[:,i], c_recall[:,i], label='recall')
+        ax.plot(s_preds[:,i], c_f1[:,i], label='F1 macro')
+        ax.set_xlabel('threshold')
+        ax.set_ylabel('score')
+        ax.legend()
     plt.tight_layout()
     plt.show()
