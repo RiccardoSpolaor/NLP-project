05957e7de9d4ea0782b2d59639cf42ef03dc36a9
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-05-14 18:27:52 +0200

    Update dataset_builder.py
    Add augmentation during train-test split

--- src/datasets/dataset_builder.py
+++ src/datasets/dataset_builder.py
@@ -86,9 +86,11 @@ def get_dataframes(
 
 def split_dataframes(
     arguments_df: pd.DataFrame, labels_df: pd.DataFrame, seed: int = 42,
-    test_size: int = .2) -> Tuple[Tuple[pd.DataFrame, pd.DataFrame],
-                                  Tuple[pd.DataFrame, pd.DataFrame]]:
+    test_size: int = .2, augmented_premises_file: Optional[str] = None
+    ) -> Tuple[Tuple[pd.DataFrame, pd.DataFrame],
+               Tuple[pd.DataFrame, pd.DataFrame]]:
     """Split the arguments and label dataframes into train and validation.
+    Apply augmentation if the file of augmented premises is provided.
 
     Parameters
     ----------
@@ -100,6 +102,9 @@ def split_dataframes(
         The seed to use, by default 42.
     test_size : int, optional
         The test size, by default 0.2.
+    augmented_premises_file : str, optional
+        The file containing the augmented premises for data augmentation,
+        by default None.
 
     Returns
     -------
@@ -110,6 +115,33 @@ def split_dataframes(
         The split arguments validation dataframe and the respective
         labels dataframe.
     """
+    if augmented_premises_file is not None:
+        (arguments_df_1, arguments_df_2, labels_df_1, labels_df_2, idx_1, _
+         ) = train_test_split(
+             arguments_df, labels_df, arguments_df.index.values)
+         
+        augmented_df = arguments_df[
+            [('Stimulation' in label) or ('Hedonism' in label) or 
+             ('Conformity: interpersonal' in label) for label
+             in arguments_df['Labels']]]
+        
+        with open(augmented_premises_file) as f:
+            augmented_premises = f.readlines()
+
+        augmented_df['Premise'] = augmented_premises
+        
+        arguments_df_1 = pd.concat(
+            (arguments_df_1,
+             augmented_df.loc[[index for index in augmented_df.index if index in idx_1]]
+             ), axis=0)
+        
+        labels_df_1 = pd.concat(
+            (labels_df_1,
+             labels_df.loc[[index for index in augmented_df.index if index in idx_1]]
+             ), axis=0)
+
+        return (arguments_df_1, labels_df_1), (arguments_df_2, labels_df_2)
+
     # Split the dataframes in train and validation
     arguments_df_1, arguments_df_2, labels_df_1, labels_df_2 = \
         train_test_split(arguments_df, labels_df, test_size=test_size,
